f116c974f5853a1ef5198b26f768906bf799a488
Marcus Holmersson <mahw17@student.bth.se>
2018-12-03 20:44:30 +0100

    updated mahw17/weather to v1.0.3

--- config/apikey_sample.php
+++ config/apikey_sample.php
@@ -2,20 +2,21 @@
 /**
  * Storage for API keys.
  *
+ * If you change file name you must update config/weather with the new file name.
  */
 
- return [
-     // API KEY for https://ipstack.com/documentation
-     'ipstack' => [
-         "apiKey"        => "novalid26f994350ae2943e96aa8a",
-         "apiUrl"        => "http://api.ipstack.com/",
-         "apiExample"    => "http://api.ipstack.com/134.201.250.155?access_key=novalid26f994350ae2943e96aa8a"
-     ],
-     // API KEY for https://darksky.net/dev
-     'darksky' => [
-         "apiKey"        => "novalid26f994350ae2943e96aa8a",
-         "apiUrl"        => "https://api.darksky.net/forecast/",
-         "apiExtension"  => "?lang=sv&units=si&exclude=minutely,hourly,alerts,flags",
-         "apiExample"    => "https://api.darksky.net/forecast/novalid26f994350ae2943e96aa8a/37.8267,-122.4233"
-     ]
- ];
+return [
+    // API KEY for https://ipstack.com/documentation
+    'ipstack' => [
+        "apiKey"        => "novalid26f994350ae2943e96aa8a",
+        "apiUrl"        => "http://api.ipstack.com/",
+        "apiExample"    => "http://api.ipstack.com/134.201.250.155?access_key=novalid26f994350ae2943e96aa8a"
+    ],
+    // API KEY for https://darksky.net/dev
+    'darksky' => [
+        "apiKey"        => "novalid26f994350ae2943e96aa8a",
+        "apiUrl"        => "https://api.darksky.net/forecast/",
+        "apiExtension"  => "?lang=sv&units=si&exclude=minutely,hourly,alerts,flags",
+        "apiExample"    => "https://api.darksky.net/forecast/novalid26f994350ae2943e96aa8a/37.8267,-122.4233"
+    ]
+];
